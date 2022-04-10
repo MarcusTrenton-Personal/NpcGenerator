441846d4587abec92c3bc80e875a2b2e50f67dca
mtrenton <marcus.trenton@gmail.com>
2022-04-10 14:09:21 -0700

    Added Quantity and Generate input stubs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,6 +27,14 @@ namespace NpcGenerator
             InitializeComponent();
         }
 
+        bool IsPositiveNumber(string text)
+        {
+            int result;
+            bool isInt = int.TryParse(text, out result);
+            bool isNaturalNumber = isInt && result > 0;
+            return isNaturalNumber;
+        }
+
         private void ChooseConfiguration(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -36,6 +44,36 @@ namespace NpcGenerator
             {
                 configurationPath.Text = openFileDialog.FileName;
             }
+        }
+
+        private void NpcQuantityInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsPositiveNumber(e.Text);
+        }
+
+        private void NpcQuantityPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsPositiveNumber(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void GenerateNpcs(object sender, RoutedEventArgs e)
+        {
+            //TODO:
+            //Validate input. Seperate handlers?
+            //Parse configuration
+            //Generate NPCs' CSV
+            //Display CSV
         }
     }
 }
