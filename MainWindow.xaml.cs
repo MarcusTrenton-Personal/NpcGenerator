3571e54f6ffe061f7f765e4f7fe5db79552d05d9
mtrenton <marcus.trenton@gmail.com>
2022-04-11 20:17:06 -0700

    Added stub result text block. Added validation for inputs. The 'Generate NPCs' button is disabled until there is valid input.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,12 +28,24 @@ namespace NpcGenerator
             InitializeComponent();
         }
 
-        bool IsPositiveNumber(string text)
+        bool TryParsePositiveNumber(string text, out int result)
         {
-            int result;
             bool isInt = int.TryParse(text, out result);
             bool isNaturalNumber = isInt && result > 0;
             return isNaturalNumber;
+        }
+
+        void UpdateGenerateButtonEnabled()
+        {
+            if(generateButton != null && configurationPathText != null && npcQuantityText != null)
+            {
+                bool isFilePicked = !String.IsNullOrEmpty(configurationPathText.Content.ToString());
+
+                int npcQuantity;
+                bool isNpcQuantityPositiveInteger = TryParsePositiveNumber(npcQuantityText.Text, out npcQuantity);
+
+                generateButton.IsEnabled = isFilePicked && isNpcQuantityPositiveInteger;
+            }
         }
 
         private void ChooseConfiguration(object sender, RoutedEventArgs e)
@@ -42,13 +55,22 @@ namespace NpcGenerator
             bool? filePicked = openFileDialog.ShowDialog();
             if(filePicked == true)
             {
-                configurationPath.Content = openFileDialog.FileName;
+
+                configurationPathText.Content = openFileDialog.FileName;
+                UpdateGenerateButtonEnabled();
             }
         }
 
         private void NpcQuantityInput(object sender, TextCompositionEventArgs e)
         {
-            e.Handled = !IsPositiveNumber(e.Text);
+            int unusedResult;
+            e.Handled = !TryParsePositiveNumber(e.Text, out unusedResult);
+            UpdateGenerateButtonEnabled();
+        }
+
+        private void NpcQuantityInputChanged(object sender, TextChangedEventArgs args)
+        {
+            UpdateGenerateButtonEnabled();
         }
 
         private void NpcQuantityPasting(object sender, DataObjectPastingEventArgs e)
@@ -56,7 +78,8 @@ namespace NpcGenerator
             if (e.DataObject.GetDataPresent(typeof(String)))
             {
                 String text = (String)e.DataObject.GetData(typeof(String));
-                if (!IsPositiveNumber(text))
+                int unusedResult;
+                if (!TryParsePositiveNumber(text, out unusedResult))
                 {
                     e.CancelCommand();
                 }
@@ -65,15 +88,65 @@ namespace NpcGenerator
             {
                 e.CancelCommand();
             }
+
+            UpdateGenerateButtonEnabled();
+        }
+
+        private bool ValidateInput(out int npcQuantity, ref string configurationPath)
+        {
+            npcQuantity = 0;
+
+            configurationPath = configurationPathText.Content.ToString();
+            bool configurationFileExists = File.Exists(configurationPath);
+            if (!configurationFileExists)
+            {
+                MessageBox.Show("Configuration path is not valid");
+                return false;
+            }
+            try
+            {
+                string text = File.ReadAllText(configurationPath);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Cannot read configuration file. Try a different file.");
+                return false;
+            }
+
+            bool isNpcQuantityValid = TryParsePositiveNumber(npcQuantityText.Text, out npcQuantity);
+            if (!isNpcQuantityValid)
+            {
+                MessageBox.Show("NPC Quantity must be at least 1");
+                return false;
+            }
+
+            return true;
         }
 
         private void GenerateNpcs(object sender, RoutedEventArgs e)
         {
-            //TODO:
-            //Validate input. Seperate handlers?
-            //Parse configuration
-            //Generate NPCs' CSV
-            //Display CSV
+            int npcQuantity;
+            string configurationPath = "";
+            bool isValid = ValidateInput(out npcQuantity, ref configurationPath);
+            if(!isValid)
+            {
+                return;
+            }
+
+            //TODO: Parse configuration
+
+            //TODO: Generate NPCs' CSV
+            GeneratedText.Text = "";
+            for(int i = 0; i < npcQuantity; ++i)
+            {
+                GeneratedText.Text += "INSERT NPC HERE!";
+                if(i+1 < npcQuantity)
+                {
+                    GeneratedText.Text += "\n";
+                }
+            }
+
+            //TODO: Display CSV
         }
     }
 }
