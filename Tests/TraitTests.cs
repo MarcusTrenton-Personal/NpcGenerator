99327ac26d01e7e636fc455a2a1747115e869fb8
mtrenton <marcus.trenton@gmail.com>
2022-04-23 00:34:43 -0700

    Added unit test for randomness

---
+++ Tests/TraitTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NpcGenerator;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class TraitTests
+    {
+        const string HEADS = "Heads";
+        const string TAILS = "Tails";
+
+        [TestMethod]
+        public void TraitSelectionIsRandom()
+        {
+            TraitCategory category = new TraitCategory("Coin");
+            category.Add(new Trait(HEADS, 1));
+            category.Add(new Trait(TAILS, 1));
+            category.SetRandomizer(new Random());
+
+            int headCount = 0;
+            int tailCount = 0;
+            const int ROLL_COUNT = 100;
+
+            for(int i = 0; i < ROLL_COUNT; ++i)
+            {
+                string choice = category.Choose();
+                switch(choice)
+                {
+                    case HEADS:
+                        headCount++;
+                        break;
+
+                    case TAILS:
+                        tailCount++;
+                        break;
+
+                    default:
+                        Assert.Fail("Trait returned value " + choice + " that was not added");
+                        break;
+                }
+            }
+
+            Assert.IsTrue(headCount > 0, HEADS + " never came up after " + ROLL_COUNT + " flips");
+            Assert.IsTrue(tailCount > 0, TAILS + " never came up after " + ROLL_COUNT + " flips");
+        }
+    }
+}
