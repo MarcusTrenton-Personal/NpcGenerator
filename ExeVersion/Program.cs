1be2a3e76993d09dc7be5f66c189fd8219778f24
mtrenton <marcus.trenton@gmail.com>
2022-05-13 20:43:27 -0700

    Hardened ExeVersion against bad input. Also removed the unneeded, overcomplicated way of extracting part of the version. Now returns the production version as the file version can be too much info (using Notepad.exe as an example).

--- ExeVersion/Program.cs
+++ ExeVersion/Program.cs
@@ -1,34 +1,37 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace ExeVersion
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            if(args.Length == 0 || string.IsNullOrEmpty(args[0]))
+            if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
             {
                 Console.Error.WriteLine("Missing parameter for exe path");
+                PrintUsage();
                 Environment.Exit(2);
             }
 
-            var versionInfo = FileVersionInfo.GetVersionInfo(args[0]);
-            if(args.Length >= 2 && args[1] == "-Major.Minor")
+            try
             {
-                string[] parts = versionInfo.FileVersion.Split('.');
-                if(parts.Length < 2)
-                {
-                    Console.Error.WriteLine("Version is malformed with less than 2 parts.");
-                    Environment.Exit(2);
-                }
-                Console.Out.Write(parts[0]+"."+parts[1]);
+                FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(args[0]);
+                Console.Out.Write(versionInfo.ProductVersion);
             }
-            else
+            catch(FileNotFoundException)
             {
-                Console.Out.Write(versionInfo.FileVersion);
+                Console.Error.WriteLine("File " + args[0] + " not found.");
+                PrintUsage();
+                Environment.Exit(2);
             }
-            
+        }
+
+        private static void PrintUsage()
+        {
+            Console.Error.WriteLine("Usage: ExeVersion <path>");
+            Console.Error.WriteLine("Example: ExeVersion C:\\Windows\\System32\\notepad.exe");
         }
     }
 }
