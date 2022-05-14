7558e942ddc0ab3c6e2c98d7db299d1194028c58
mtrenton <marcus.trenton@gmail.com>
2022-05-14 16:09:01 -0700

    PublishAndTag.bat will not tag if the publish fails.

--- PublishAndTag.bat
+++ PublishAndTag.bat
@@ -1,6 +1,7 @@
 @echo off
 
 dotnet publish "NpcGenerator\NpcGenerator.csproj" /p:"PublishProfile=\NpcGenerator\Properties\PublishProfiles\Public.pubxml"
+if errorlevel 1 exit /B errorlevel
 
 for /f %%i in ('ExeVersion\bin\Release\netcoreapp3.1\ExeVersion NpcGenerator\bin\Release\netcoreapp3.1\publish\NpcGenerator.exe') do set VERSION=%%i
 
