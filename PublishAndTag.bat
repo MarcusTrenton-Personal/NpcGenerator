476a662a6d27d68accff71a1451d551401609d50
mtrenton <marcus.trenton@gmail.com>
2022-05-18 23:14:22 -0700

    Commented the weird variable assignment required in batch files.

--- PublishAndTag.bat
+++ PublishAndTag.bat
@@ -3,6 +3,7 @@
 dotnet publish "NpcGenerator\NpcGenerator.csproj" /p:"PublishProfile=\NpcGenerator\Properties\PublishProfiles\Public.pubxml"
 if errorlevel 1 exit /B errorlevel
 
+REM Sadly batch files cannot directly assign the output of a command to a variable. This is an industry-standard work-around hack.
 for /f %%i in ('ExeVersion\bin\Release\netcoreapp3.1\ExeVersion NpcGenerator\bin\Release\netcoreapp3.1\publish\NpcGenerator.exe') do set VERSION=%%i
 
 git tag v%VERSION%
