013279ecbba06b22917dde2623dadd3d6062eb19
mtrenton <marcus.trenton@gmail.com>
2022-06-04 10:16:53 -0700

    Removed AssemblyInfo from source code folder. It should be in the GeneratedCodeFolder.

--- NpcGenerator/SourceCode/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.Reflection;
-
-[assembly: AssemblyVersion("1.3.885.1023")]
-[assembly: AssemblyDescription("Randomly generate NPCs' demographics")]
-[assembly: AssemblyCopyright("Marcus Trenton 2022")]
-[assembly: AssemblyProduct("NPC Generator")]
-[assembly: AssemblyTitle("NPC Generator") ]
-
