5d511505f4f0eb7eff9bd886bf10abf57b441792
mtrenton <marcus.trenton@gmail.com>
2022-09-08 21:56:35 -0700

    Added DirectedArrowGraph class for detecting cycles. Tests prove it works.

---
+++ Services/DirectedArrowGraph.cs
@@ -0,0 +1,163 @@
+﻿/*Copyright(C) 2022 Marcus Trenton, marcus.trenton@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public class DirectedArrowGraph<T> where T : notnull
+    {
+        public bool AddNode(T node)
+        {
+            if (node is null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            if (m_nodeEdges.ContainsKey(node))
+            {
+                return false;
+            }
+
+            m_nodeEdges[node] = new HashSet<T>();
+            return true;
+        }
+
+        public bool AddEdge(T start, T end)
+        {
+            if (start is null)
+            {
+                throw new ArgumentNullException(nameof(start));
+            }
+            if (end is null)
+            {
+                throw new ArgumentNullException(nameof(end));
+            }
+
+            AddNodeIfNotAlreadyContained(start);
+            AddNodeIfNotAlreadyContained(end);
+
+            if (ReferenceEquals(start, end) || start.Equals(end))
+            {
+                return false;
+            }
+
+            bool successful = m_nodeEdges[start].Add(end);
+            return successful;
+        }
+
+        private void AddNodeIfNotAlreadyContained(T node)
+        {
+            bool hasNode = m_nodeEdges.ContainsKey(node);
+            if (!hasNode)
+            {
+                AddNode(node);
+            }
+        }
+
+        public bool HasCycle(out List<T> path)
+        {
+            path = null;
+
+            Dictionary<T, CycleMarker> markers = new Dictionary<T, CycleMarker>(m_nodeEdges.Count);
+            foreach (T node in m_nodeEdges.Keys)
+            {
+                markers[node] = new CycleMarker();
+            }
+
+            foreach (T node in m_nodeEdges.Keys)
+            {
+                bool visited = markers[node].Visited;
+                if (!visited)
+                {
+                    bool hasCycle = FindCycle(node, markers);
+                    if (hasCycle)
+                    {
+                        path = CyclePath(markers);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool FindCycle(T startingNode, Dictionary<T, CycleMarker> markers)
+        {
+            CycleMarker marker = markers[startingNode];
+            if (!marker.Visited)
+            {
+                marker.Visited = true;
+                marker.InRecursionPath = true;
+
+                foreach (T adjacentNode in m_nodeEdges[startingNode])
+                {
+                    if (!markers[adjacentNode].Visited)
+                    {
+                        bool isCycle = FindCycle(adjacentNode, markers);
+                        if (isCycle)
+                        {
+                            return true;
+                        }
+                    }
+                    else if (markers[adjacentNode].InRecursionPath)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            marker.InRecursionPath = false;
+            return false;
+        }
+
+        private List<T> CyclePath(Dictionary<T, CycleMarker> markers)
+        {
+            List<T> path = new List<T>();
+            foreach (T node in m_nodeEdges.Keys)
+            {
+                if (markers[node].InRecursionPath)
+                {
+                    T nextNode = node;
+                    do
+                    {
+                        path.Add(nextNode);
+                        foreach (T candidateNode in m_nodeEdges[nextNode])
+                        {
+                            if (markers[candidateNode].InRecursionPath)
+                            {
+                                nextNode = candidateNode;
+                                break;
+                            }
+                        }
+                    }
+                    while (!path.Contains(nextNode));
+                    path.Add(nextNode); //Close the loop
+                }
+                break;
+            }
+            return path;
+        }
+
+        private class CycleMarker
+        {
+            public bool Visited { get; set; } = false;
+            public bool InRecursionPath { get; set; } = false;
+        }
+
+        private readonly Dictionary<T,HashSet<T>> m_nodeEdges = new Dictionary<T,HashSet<T>>();
+    }
+}
---
+++ ServicesTests/DirectedArrowGraphTests.cs
@@ -0,0 +1,266 @@
+﻿/*Copyright(C) 2022 Marcus Trenton, marcus.trenton@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class DirectedArrowGraphTests
+    {
+        [TestMethod]
+        public void AddNewIntNode()
+        {
+            DirectedArrowGraph<int> graph = new DirectedArrowGraph<int>();
+
+            bool success = graph.AddNode(1);
+            
+            Assert.IsTrue(success, "Failed to add node to empty graph");
+        }
+
+        [TestMethod]
+        public void AddNewObjectNode()
+        {
+            DirectedArrowGraph<TestObject> graph = new DirectedArrowGraph<TestObject>();
+
+            bool success = graph.AddNode(new TestObject());
+
+            Assert.IsTrue(success, "Failed to add node to empty graph");
+        }
+
+        [TestMethod]
+        public void AddMultipleNodes()
+        {
+            DirectedArrowGraph<float> graph = new DirectedArrowGraph<float>();
+
+            float[] values = new float[] { 1.5f, 2.4f, 3.3f, 4.2f, 5.1f };
+            foreach (float value in values)
+            {
+                bool success = graph.AddNode(value);
+                Assert.IsTrue(success, "Failed to add node to graph");
+            }
+        }
+
+        [TestMethod]
+        public void AddRedundantNode()
+        {
+            DirectedArrowGraph<string> graph = new DirectedArrowGraph<string>();
+
+            const string VALUE = "Chocolate";
+            graph.AddNode(new string(VALUE));
+            bool success = graph.AddNode(new string(VALUE));
+
+            Assert.IsFalse(success, "Incorrectly added redundant node");
+        }
+
+        [TestMethod]
+        public void AddNullNode()
+        {
+            DirectedArrowGraph<StringBuilder> graph = new DirectedArrowGraph<StringBuilder>();
+
+            bool threwExcepion = false;
+            try
+            {
+                bool success = graph.AddNode(null);
+            }
+            catch (ArgumentNullException)
+            {
+                threwExcepion = true;
+            }
+
+            Assert.IsTrue(threwExcepion, "Failed to throw ArgumentNullException");
+        }
+
+        [TestMethod]
+        public void AddEdgeNullStart()
+        {
+            DirectedArrowGraph<string> graph = new DirectedArrowGraph<string>();
+
+            bool threwExcepion = false;
+            try
+            {
+                bool success = graph.AddEdge(start: null, end: "Dinosaur");
+            }
+            catch (ArgumentNullException)
+            {
+                threwExcepion = true;
+            }
+
+            Assert.IsTrue(threwExcepion, "Failed to throw ArgumentNullException");
+        }
+
+        [TestMethod]
+        public void AddEdgeNullEnd()
+        {
+            DirectedArrowGraph<string> graph = new DirectedArrowGraph<string>();
+
+            bool threwExcepion = false;
+            try
+            {
+                bool success = graph.AddEdge(start: "World", end: null);
+            }
+            catch (ArgumentNullException)
+            {
+                threwExcepion = true;
+            }
+
+            Assert.IsTrue(threwExcepion, "Failed to throw ArgumentNullException");
+        }
+
+        [TestMethod]
+        public void AddEdgeToSelf()
+        {
+            DirectedArrowGraph<string> graph = new DirectedArrowGraph<string>();
+
+            const string VALUE = "World";
+            bool success = graph.AddEdge(start: VALUE, end: VALUE);
+
+            Assert.IsFalse(success, "Incorrectly added a node with an edge to itself");
+        }
+
+        [TestMethod]
+        public void AddEdge()
+        {
+            DirectedArrowGraph<string> graph = new DirectedArrowGraph<string>();
+
+            bool success = graph.AddEdge(start: "Dinosaur", end: "Civilization");
+
+            Assert.IsTrue(success, "Failed to add an edge");
+        }
+
+        [TestMethod]
+        public void DetectIntCycleWithOnlyIsolatedNodes()
+        {
+            DirectedArrowGraph<int> graph = new DirectedArrowGraph<int>();
+
+            graph.AddNode(0);
+            graph.AddNode(1);
+            graph.AddNode(2);
+            graph.AddNode(3);
+
+            bool hasCycle = graph.HasCycle(out List<int> path);
+
+            Assert.IsFalse(hasCycle, "Incorrectly detected cycles in a graph of isolated nodes");
+            Assert.IsNull(path, "Graph without cycles should return a null path");
+        }
+
+        [TestMethod]
+        public void DetectIntCycleTrue()
+        {
+            DirectedArrowGraph<int> graph = new DirectedArrowGraph<int>();
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+
+            bool hasCycle = graph.HasCycle(out List<int> path);
+
+            Assert.IsTrue(hasCycle, "Failed detect a cycle in a graph with a cycle");
+            for (int i = 0; i < path.Count; i++)
+            {
+                int initialValue = path[i];
+                int nextValue = path[(i + 1) % path.Count];
+                bool isIncrementing = initialValue + 1 == nextValue;
+                bool isWrapAround = initialValue == 2 && nextValue == 0;
+                bool isClosedLoop = initialValue == nextValue;
+                Assert.IsTrue(isIncrementing || isWrapAround || isClosedLoop, 
+                    "Return path has incorrect link: " + initialValue + " -> " + nextValue);
+            }
+        }
+
+        [TestMethod]
+        public void DetectCycleFalse()
+        {
+            DirectedArrowGraph<int> graph = new DirectedArrowGraph<int>();
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(2, 3);
+
+            bool hasCycle = graph.HasCycle(out List<int> path);
+
+            Assert.IsFalse(hasCycle, "Incorrectly detected cycles in a graph without a cycle");
+            Assert.IsNull(path, "Graph without cycles should return a null path");
+        }
+
+        [TestMethod]
+        public void DetectCycleTrueWithMixedIsolatedNodes()
+        {
+            DirectedArrowGraph<int> graph = new DirectedArrowGraph<int>();
+
+            graph.AddNode(-1);
+            graph.AddNode(-2);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddNode(-3);
+
+            bool hasCycle = graph.HasCycle(out List<int> path);
+
+            Assert.IsTrue(hasCycle, "Failed detect a cycle in a graph with a cycle");
+            for (int i = 0; i < path.Count; i++)
+            {
+                int initialValue = path[i];
+                int nextValue = path[(i + 1) % path.Count];
+                bool isIncrementing = initialValue + 1 == nextValue;
+                bool isWrapAround = initialValue == 2 && nextValue == 0;
+                bool isClosedLoop = initialValue == nextValue;
+                Assert.IsTrue(isIncrementing || isWrapAround || isClosedLoop,
+                    "Return path has incorrect link: " + initialValue + " -> " + nextValue);
+            }
+        }
+
+        [TestMethod]
+        public void DetectObjectCycleTrue()
+        {
+            DirectedArrowGraph<string> graph = new DirectedArrowGraph<string>();
+
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "C");
+            graph.AddEdge("C", "A");
+            graph.AddEdge("C", "D");
+
+            bool hasCycle = graph.HasCycle(out List<string> path);
+
+            Assert.IsTrue(hasCycle, "Failed detect a cycle in a graph with a cycle");
+            for (int i = 0; i < path.Count; i++)
+            {
+                string initialValue = path[i];
+                string nextValue = path[(i + 1) % path.Count];
+                bool isAB = initialValue == "A" && nextValue == "B";
+                bool isBC = initialValue == "B" && nextValue == "C";
+                bool isCA = initialValue == "C" && nextValue == "A";
+                bool isClosedLoop = initialValue == nextValue;
+                Assert.IsTrue(isAB || isBC || isCA || isClosedLoop,
+                    "Return path has incorrect link: " + initialValue + " -> " + nextValue);
+            }
+        }
+
+        private struct TestObject
+        {
+#pragma warning disable CS0169 // Field is never used. Don't care as it's a test object. 
+            int x;
+            string y;
+#pragma warning restore CS0169 // Field is never used
+        }
+    }
+}
