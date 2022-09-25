ffa815a5300e2f55478c51445432d8b6a3963867
mtrenton <marcus.trenton@gmail.com>
2022-09-24 18:54:36 -0700

    Added test for a circular dependency of chained bonus selections and chained requirements.

--- NpcGeneratorTests/ConfigurationParser/JsonConfigurationParserTests/RequirementJsonConfigurationParserTests.cs
+++ NpcGeneratorTests/ConfigurationParser/JsonConfigurationParserTests/RequirementJsonConfigurationParserTests.cs
@@ -1122,5 +1122,149 @@ namespace Tests.JsonConfigurationParserTests
 
             Assert.IsTrue(threwException, "Failed to throw CircularRequirementsException");
         }
+
+        [TestMethod]
+        public void RequirementChainedBonusSelectionAndRequirementCircularRequirementsException()
+        {
+            const string CATEGORY_A = "Animal";
+            const string TRAIT_A = "Bear";
+            const string CATEGORY_B = "Building";
+            const string TRAIT_B = "School";
+            const string CATEGORY_C = "Colour";
+            const string TRAIT_C = "Blue";
+            const string CATEGORY_D = "Demeanor";
+            const string TRAIT_D = "Arrogant";
+            const string CATEGORY_E = "Energy";
+            const string TRAIT_E = "Nuclear";
+            const string CATEGORY_F = "Fear";
+            const string TRAIT_F = "Spiders";
+
+            string text = $@"{{
+                'trait_categories' : [
+                    {{
+                        'name' : '{CATEGORY_A}',
+                        'selections': 1,
+                        'requirements' : {{
+                            'category_name' : '{CATEGORY_F}',
+				            'trait_name' : '{TRAIT_F}'
+			            }},
+                        'traits' : [
+                            {{ 
+                                'name' : '{TRAIT_A}', 
+                                'weight' : 1,
+                                'bonus_selection' : {{ 'trait_category_name' : '{CATEGORY_B}', 'selections' : 1 }}
+                            }}
+                        ]
+                    }},
+                    {{
+                        'name' : '{CATEGORY_B}',
+                        'selections': 1,
+                        'traits' : [
+                            {{ 
+                                'name' : '{TRAIT_B}', 
+                                'weight' : 1,
+                                'bonus_selection' : {{ 'trait_category_name' : '{CATEGORY_C}', 'selections' : 1 }}
+                            }}
+                        ]
+                    }},
+                    {{
+                        'name' : '{CATEGORY_C}',
+                        'selections': 1,
+                        'traits' : [
+                            {{ 
+                                'name' : '{TRAIT_C}', 
+                                'weight' : 1
+                            }}
+                        ]
+                    }},
+                    {{
+                        'name' : '{CATEGORY_D}',
+                        'selections': 1,
+                        'requirements' : {{
+                            'category_name' : '{CATEGORY_C}',
+				            'trait_name' : '{TRAIT_C}'
+			            }},
+                        'traits' : [
+                            {{ 
+                                'name' : '{TRAIT_D}', 
+                                'weight' : 1,
+                                'bonus_selection' : {{ 'trait_category_name' : '{CATEGORY_E}', 'selections' : 1 }}
+                            }}
+                        ]
+                    }},
+                    {{
+                        'name' : '{CATEGORY_E}',
+                        'selections': 1,
+                        'traits' : [
+                            {{ 
+                                'name' : '{TRAIT_E}', 
+                                'weight' : 1,
+                                'bonus_selection' : {{ 'trait_category_name' : '{CATEGORY_F}', 'selections' : 1 }}
+                            }}
+                        ]
+                    }},
+                    {{
+                        'name' : '{CATEGORY_F}',
+                        'selections': 1,
+                        'traits' : [
+                            {{ 
+                                'name' : '{TRAIT_F}', 
+                                'weight' : 1
+                            }}
+                        ]
+                    }}
+                ]
+            }}";
+
+            JsonConfigurationParser parser = new JsonConfigurationParser(SCHEMA_PATH);
+
+            bool threwException = false;
+            try
+            {
+                TraitSchema schema = parser.Parse(text);
+            }
+            catch (CircularRequirementsException exception)
+            {
+                Assert.AreEqual(6, exception.Cycle.Count, "Wrong number of links in the CircularRequirementsException");
+                foreach (TraitSchema.Dependency cycle in exception.Cycle)
+                {
+                    if (cycle.DependentCategory == CATEGORY_A)
+                    {
+                        Assert.AreEqual(TraitSchema.Dependency.Type.Requirement, cycle.DependencyType, "Wrong dependency type");
+                        Assert.AreEqual(CATEGORY_F, cycle.OriginalCategory, "Wrong DependentCategory");
+                    }
+                    else if (cycle.DependentCategory == CATEGORY_B)
+                    {
+                        Assert.AreEqual(TraitSchema.Dependency.Type.BonusSelection, cycle.DependencyType, "Wrong dependency type");
+                        Assert.AreEqual(CATEGORY_A, cycle.OriginalCategory, "Wrong DependentCategory");
+                    }
+                    else if (cycle.DependentCategory == CATEGORY_C)
+                    {
+                        Assert.AreEqual(TraitSchema.Dependency.Type.BonusSelection, cycle.DependencyType, "Wrong dependency type");
+                        Assert.AreEqual(CATEGORY_B, cycle.OriginalCategory, "Wrong DependentCategory");
+                    }
+                    else if (cycle.DependentCategory == CATEGORY_D)
+                    {
+                        Assert.AreEqual(TraitSchema.Dependency.Type.Requirement, cycle.DependencyType, "Wrong dependency type");
+                        Assert.AreEqual(CATEGORY_C, cycle.OriginalCategory, "Wrong DependentCategory");
+                    }
+                    else if (cycle.DependentCategory == CATEGORY_E)
+                    {
+                        Assert.AreEqual(TraitSchema.Dependency.Type.BonusSelection, cycle.DependencyType, "Wrong dependency type");
+                        Assert.AreEqual(CATEGORY_D, cycle.OriginalCategory, "Wrong DependentCategory");
+                    }
+                    else
+                    {
+                        Assert.AreEqual(CATEGORY_F, cycle.DependentCategory, "Wrong OriginalCategory");
+                        Assert.AreEqual(TraitSchema.Dependency.Type.BonusSelection, cycle.DependencyType, "Wrong dependency type");
+                        Assert.AreEqual(CATEGORY_E, cycle.OriginalCategory, "Wrong DependentCategory");
+                    }
+                }
+
+                threwException = true;
+            }
+
+            Assert.IsTrue(threwException, "Failed to throw CircularRequirementsException");
+        }
     }
 }
